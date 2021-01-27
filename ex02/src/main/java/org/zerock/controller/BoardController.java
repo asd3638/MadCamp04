69f2f94626994e0af46c362dca45965caa41b223
jsj7878 <61611131+jsj7878@users.noreply.github.com>
2021-01-27 21:28:01 +0900

    save for another branch

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -15,30 +15,30 @@
  * 
  * @Controller
  * 
- * @RequiredArgsConstructor //이러면 final만 잘 명시하면 된다.
+ * @RequiredArgsConstructor //�씠�윭硫� final留� �옒 紐낆떆�븯硫� �맂�떎.
  * 
  * @RequestMapping("/board/*")
  * 
  * @Log4j public class BoardController {
  * 
- * //비지니스화 했으니까 이제 더 이상 mapper를 보면 안된다. private final BoardService service;
+ * //鍮꾩��땲�뒪�솕 �뻽�쑝�땲源� �씠�젣 �뜑 �씠�긽 mapper瑜� 蹂대㈃ �븞�맂�떎. private final BoardService service;
  * 
- * //게시판의 정보는 db로부터 불러와야 하기 때문에 무조건 Model을 써야한다. //model을 통해서 데이터를 넣는 것은
- * addattribute를 사용한다.
+ * //寃뚯떆�뙋�쓽 �젙蹂대뒗 db濡쒕��꽣 遺덈윭���빞 �븯湲� �븣臾몄뿉 臾댁“嫄� Model�쓣 �뜥�빞�븳�떎. //model�쓣 �넻�빐�꽌 �뜲�씠�꽣瑜� �꽔�뒗 寃껋�
+ * addattribute瑜� �궗�슜�븳�떎.
  * 
  * @GetMapping("list") public void list(Criteria cri, Model model) {
  * 
  * log.info("..........listwithpaging..........."); log.info(cri);
  * log.info("..........listwithpaging...........");
  * 
- * //service의 getlist메소드를 이용해서 데이터를 받아와서 medel에 저장해두는 것이다.
+ * //service�쓽 getlist硫붿냼�뱶瑜� �씠�슜�빐�꽌 �뜲�씠�꽣瑜� 諛쏆븘���꽌 medel�뿉 ���옣�빐�몢�뒗 寃껋씠�떎.
  * model.addAttribute("list", service.getList(cri));
  * model.addAttribute("pageMaker", new PageDTO(cri,
- * service.getTotalCount(cri))); } //등록
+ * service.getTotalCount(cri))); } //�벑濡�
  * 
  * @GetMapping("register") public void registerGet() { }
  * 
- * //페이지에서 액션 버튼 이벤트가 발생하면 postmapping 실행
+ * //�럹�씠吏��뿉�꽌 �븸�뀡 踰꾪듉 �씠踰ㅽ듃媛� 諛쒖깮�븯硫� postmapping �떎�뻾
  * 
  * @PostMapping("register") public String register(BoardVO board,
  * RedirectAttributes rttr) {
@@ -48,22 +48,22 @@
  * 
  * rttr.addFlashAttribute("result", bno);
  * 
- * return "redirect:/board/list"; } //수정 (modify를 누르면 get 으로)
+ * return "redirect:/board/list"; } //�닔�젙 (modify瑜� �늻瑜대㈃ get �쑝濡�)
  * 
  * @PostMapping("modify") public String modify(BoardVO board, Criteria cri,
  * RedirectAttributes rttr) {
  * 
- * log.info("board: " + board); int count = service.modify(board); //수정이 됐는지 안
- * 됐는지 return값을 가지고 확인을 항상 해야한다. if (count == 1) {
+ * log.info("board: " + board); int count = service.modify(board); //�닔�젙�씠 �릱�뒗吏� �븞
+ * �릱�뒗吏� return媛믪쓣 媛�吏�怨� �솗�씤�쓣 �빆�긽 �빐�빞�븳�떎. if (count == 1) {
  * rttr.addFlashAttribute("result", "modify"); }
  * 
- * //post 방식은 view에서 데이터를 달고 다닌다고 redirection에 전달되진 않으니까 분기 메소드에서 addattribute로
- * 데이터를 마저 달아줘야 한다. rttr.addAttribute("pageNum", cri.getPageNum());
+ * //post 諛⑹떇�� view�뿉�꽌 �뜲�씠�꽣瑜� �떖怨� �떎�땶�떎怨� redirection�뿉 �쟾�떖�릺吏� �븡�쑝�땲源� 遺꾧린 硫붿냼�뱶�뿉�꽌 addattribute濡�
+ * �뜲�씠�꽣瑜� 留덉� �떖�븘以섏빞 �븳�떎. rttr.addAttribute("pageNum", cri.getPageNum());
  * rttr.addAttribute("amount", cri.getAmount());
  * 
  * return "redirect:/board/list"; }
  * 
- * //삭제
+ * //�궘�젣
  * 
  * @PostMapping("remove") public String remove(Long bno, Criteria cri,
  * RedirectAttributes rttr) { int count = service.remove(bno);
@@ -71,13 +71,13 @@
  * if (count == 1) { rttr.addFlashAttribute("result", "remove"); }
  * 
  * rttr.addAttribute("pageNum", cri.getPageNum()); rttr.addAttribute("amount",
- * cri.getAmount()); //검색 타입이 추가되어야한다. //rttr.addAttribute("pageNum",
+ * cri.getAmount()); //寃��깋 ���엯�씠 異붽��릺�뼱�빞�븳�떎. //rttr.addAttribute("pageNum",
  * cri.getType()); //rttr.addAttribute("amount", cri.getKeyword());
  * 
  * return "redirect:/board/list"; }
  * 
- * //조회 //mapping 대상에 2개의 페이지로 분기시켰는데 원래 return값이 void이기 때문에 get이면 get으로 modify는
- * modify로 넘어간다. //@ModelAttribute를 쓰면 명시적으로 cri라는 이름으로 Criteria를 저장할 수 있다.
+ * //議고쉶 //mapping ���긽�뿉 2媛쒖쓽 �럹�씠吏�濡� 遺꾧린�떆耳곕뒗�뜲 �썝�옒 return媛믪씠 void�씠湲� �븣臾몄뿉 get�씠硫� get�쑝濡� modify�뒗
+ * modify濡� �꽆�뼱媛꾨떎. //@ModelAttribute瑜� �벐硫� 紐낆떆�쟻�쑝濡� cri�씪�뒗 �씠由꾩쑝濡� Criteria瑜� ���옣�븷 �닔 �엳�떎.
  * 
  * @GetMapping({"get", "modify"}) public void get(Long
  * bno, @ModelAttribute("cri") Criteria cri, Model model) {
@@ -113,7 +113,7 @@ public class BoardController {
 
 	@GetMapping("/register")
 	public void register() {
-
+		
 	}
 
 	// @GetMapping("/list")
